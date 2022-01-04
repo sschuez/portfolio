01e244227b75ea331551a9107b214ffa96f247af
Stephen Schuez <stephen.schuez@gmail.com>
2022-01-04 17:48:14 +0100

    added collapsable accordion to Work Experience

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,10 +1,9 @@
-// Dark Mode Toggle
+// DARK MODE
 function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
 
-  
 // CURRENT AGE CALCULATOR
 var dob = new Date("08/17/1988");  
 //calculate month difference from current date in time  
@@ -17,3 +16,39 @@ var year = age_dt.getUTCFullYear();
 var age = Math.abs(year - 1970);  
 //display the calculated age  
 document.querySelector(".my-age").innerHTML = `🙋‍♂️ ${age} years old`; 
+
+// ACCORDION
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+    
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+  //       panel.style.display = "none";
+  //     } else {
+    //       panel.style.display = "block";
+    //     }
+    //   });
+  // } 
+  
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+      
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
