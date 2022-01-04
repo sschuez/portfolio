c79e8ceb4eb30de0d63a792abbb5e96dead79d6f
Stephen Schuez <stephen.schuez@gmail.com>
2022-01-04 12:17:37 +0100

    added content to grid layout

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,20 +5,15 @@ function darkMode() {
 }
 
   
+// CURRENT AGE CALCULATOR
 var dob = new Date("08/17/1988");  
 //calculate month difference from current date in time  
 var month_diff = Date.now() - dob.getTime();  
-      
 //convert the calculated difference in date format  
 var age_dt = new Date(month_diff);
-      
 //extract year from date      
 var year = age_dt.getUTCFullYear();  
-      
 //now calculate the age of the user  
 var age = Math.abs(year - 1970);  
-      
 //display the calculated age  
-document.querySelector(".my-age").innerHTML = "${}" 
-
-// ("Age of the date entered: " + age + " years");  
+document.querySelector(".my-age").innerHTML = `🙋‍♂️ ${age} years old`; 
